67f18154ef97a5a4dfba16d23e94e8c5bfe73d74
lukmaan-b <lukmaan.butt@gmail.com>
2020-09-25 16:42:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const handleChange = ({ target }) => {
+    setTodo(target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos((prevState) => [...prevState, todo]);
+    setTodo("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={todo} onChange={handleChange} />
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
